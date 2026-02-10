d7edf4547d28824a8ce735483f33d847d9fedc9d
CosmicScale <cosmicsocial@icloud.com>
2026-02-10 23:37:15 +0000

    Fix invalid input prompt

--- PSBBN-Definitive-Patch.sh
+++ PSBBN-Definitive-Patch.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
+#
+#    PSBBN Definitive Project
+#    Copyright (C) 2004-2026 CosmicScale
+#
+#    <https://github.com/CosmicScale/PSBBN-Definitive-English-Patch>
+#
+#    SPDX-License-Identifier: GPL-3.0-or-later
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Check if the shell is bash
 if [ -z "$BASH_VERSION" ]; then
@@ -593,8 +613,9 @@ while true; do
             5) option_five; display_menu ;;
             6) option_six; display_menu ;;
             q|Q) clear; break ;;
-            *) echo "                   Invalid option, please try again."
-               sleep 2 ;;
+            *) echo -n "                   Invalid option, please try again."
+               sleep 2
+               display_menu ;;
         esac
     fi
 done
